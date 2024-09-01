30be2af0594290312b0a851bf902ba37826e70ad
ZEKE <101160055+itsZekiee@users.noreply.github.com>
2024-09-01 23:19:38 +0800

    update

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Sign In & Sign Up Form Slider
+const container = document.getElementById('register__container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
