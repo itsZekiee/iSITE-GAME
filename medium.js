8cbd10e089e581bc3cadc5bdf3a09c803117ea2d
Cedie99 <johncedrickignacio965@gmail.com>
2024-09-07 00:37:59 +0800

    added chanegs

---
+++ medium.js
@@ -0,0 +1,281 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const selectors = {
+        boardContainer: document.querySelector('.board-container'),
+        board: document.querySelector('.board'),
+        moves: document.querySelector('.moves'),
+        timer: document.querySelector('.timer'),
+        start: document.querySelector('button'),
+        win: document.querySelector('.win'),
+        input: document.querySelector('#player-name'),
+        saveButton: document.querySelector('#save-leaderboard'),
+        leaderboardList: document.querySelector('#leaderboard-list'),
+        leaderboardSection: document.querySelector('.leaderboard-section')
+    };
+
+    let startbutton = document.getElementById('startBtn');
+
+    const state = {
+        gameStarted: false,
+        flippedCards: 0,
+        totalFlips: 0,
+        totalTime: 0,
+        loop: null,
+        maxMoves: 20 // Example maxMoves value; adjust as needed
+    };
+
+    const shuffle = array => {
+        const clonedArray = [...array];
+        for (let i = clonedArray.length - 1; i > 0; i--) {
+            const randomIndex = Math.floor(Math.random() * (i + 1));
+            [clonedArray[i], clonedArray[randomIndex]] = [clonedArray[randomIndex], clonedArray[i]];
+        }
+        return clonedArray;
+    };
+
+    const pickRandom = (array, items) => {
+        const clonedArray = [...array];
+        const randomPicks = [];
+        for (let i = 0; i < items; i++) {
+            const randomIndex = Math.floor(Math.random() * clonedArray.length);
+            randomPicks.push(clonedArray[randomIndex]);
+            clonedArray.splice(randomIndex, 1);
+        }
+        return randomPicks;
+    };
+
+    const generateGame = () => {
+        const dimensions = selectors.board.getAttribute('data-dimension');
+        if (dimensions % 2 !== 0) {
+            throw new Error("The dimension of the board must be an even number.");
+        }
+
+        const images = [
+            'pics/Card-Images/chose.png',
+            'pics/Card-Images/doge.png',
+            'pics/Card-Images/fire.png',
+            'pics/Card-Images/look.png',
+            'pics/Card-Images/pepe.png',
+            'pics/Card-Images/pika.png',
+            'pics/Card-Images/sponge.png',
+            'pics/Card-Images/think.png',
+            'pics/Card-Images/leo.png',
+            'pics/Card-Images/winnie.png'
+        ];
+
+        const picks = pickRandom(images, (dimensions * dimensions) / 2);
+        const items = shuffle([...picks, ...picks]);
+
+        const cards = `
+            <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+                ${items.map(item => `
+                    <div class="card">
+                        <div class="card-front"></div>
+                        <div class="card-back"><img src="${item}" alt="Card image" style="width: 100%; height: 100%;"></div>
+                    </div>
+                `).join('')}
+            </div>
+        `;
+        const parser = new DOMParser().parseFromString(cards, 'text/html');
+        selectors.board.replaceWith(parser.querySelector('.board'));
+    };
+
+    const startGame = () => {
+        startbutton.style.display = 'none';
+        console.log('Starting the game'); // Debug line
+        state.gameStarted = true;
+        selectors.start.classList.add('disabled');
+        state.loop = setInterval(() => {
+            state.totalTime++;
+            selectors.moves.innerText = `${state.totalFlips} moves`;
+            selectors.timer.innerText = `Time: ${state.totalTime} sec`;
+        }, 1000);
+    };
+
+    const flipBackCards = () => {
+        document.querySelectorAll('.card:not(.matched)').forEach(card => {
+            card.classList.remove('flipped');
+        });
+        state.flippedCards = 0;
+    };
+
+    const flipCard = (card) => {
+        if (!state.gameStarted) {
+            startGame();
+        }
+    
+        if (!card.classList.contains('flipped') && state.flippedCards < 2) {
+            card.classList.add('flipped');
+            state.flippedCards++;
+    
+            if (state.flippedCards === 2) {
+                state.totalFlips++; // Increment moves for each pair of flipped cards
+                const flippedCards = document.querySelectorAll('.flipped:not(.matched)');
+                const firstCardSrc = flippedCards[0].querySelector('.card-back img').src;
+                const secondCardSrc = flippedCards[1].querySelector('.card-back img').src;
+    
+                if (firstCardSrc === secondCardSrc) {
+                    flippedCards[0].classList.add('matched');
+                    flippedCards[1].classList.add('matched');
+                    state.flippedCards = 0;
+                } else {
+                    setTimeout(() => {
+                        flipBackCards();
+                    }, 1000);
+                }
+    
+                selectors.moves.innerText = `${state.totalFlips} moves`;
+    
+                if (state.totalFlips >= state.maxMoves && document.querySelectorAll('.card.matched').length !== document.querySelectorAll('.card').length) {
+                    setTimeout(() => {
+                        selectors.boardContainer.classList.add('flipped');
+                        selectors.win.innerHTML = `
+                            <span class="win-text">
+                                Game Over!<br />
+                                You reached the maximum of <span class="highlight">${state.maxMoves}</span> moves.
+                            </span>
+                        `;
+                        clearInterval(state.loop);
+                        selectors.start.innerText = 'Retry';
+                        selectors.start.classList.remove('disabled');
+                    }, 1000);
+                }
+            }
+        }
+    
+        if (!document.querySelectorAll('.card:not(.matched)').length) {
+            endGame();
+        }
+    };
+
+    const resetGame = () => {
+        state.gameStarted = false;
+        state.flippedCards = 0;
+        state.totalFlips = 0;
+        state.totalTime = 0;
+
+        selectors.win.innerHTML = '';
+        selectors.boardContainer.classList.remove('flipped');
+
+        selectors.start.classList.remove('disabled');
+
+        document.querySelectorAll('.card').forEach(card => {
+            card.classList.remove('flipped', 'matched');
+        });
+        location.reload();
+        generateGame();
+    };
+
+    const endGame = () => {
+        selectors.boardContainer.classList.add('flipped');
+        selectors.moves.innerText = ``;
+        selectors.timer.innerText = ``;
+        selectors.win.innerHTML = `
+            <div class="win-text">
+                You won!<br />
+                with <span class="highlight">${state.totalFlips}</span> moves<br />
+                under <span class="highlight">${state.totalTime}</span> seconds
+            </div>
+            <div class="leaderboard-input">
+                <input type="text" id="player-name" placeholder="Enter your name" />
+                <button id="save-leaderboard">Save to Leaderboard</button>
+            </div>
+            <button id="play-again" class="play-again-button">Play Again</button>
+        `;
+
+        clearInterval(state.loop);
+
+        selectors.input = document.querySelector('#player-name');
+        selectors.saveButton = document.querySelector('#save-leaderboard');
+        document.querySelector('.leaderboard-input').style.display = 'block';
+
+        const playAgainButton = document.querySelector('#play-again');
+        playAgainButton.addEventListener('click', () => {
+            resetGame();
+        });
+    };
+
+    const displayLeaderboard = () => {
+        const leaderboard = JSON.parse(localStorage.getItem('mediumLeaderboard')) || [];
+    
+        // Sort by lowest time first, and if equal, by lowest moves
+        const sortedLeaderboard = leaderboard.sort((a, b) => a.time - b.time || a.moves - b.moves);
+    
+        selectors.leaderboardList.innerHTML = sortedLeaderboard.map((entry, index) => {
+            // Highlight top 1, 2, 3 players with different styles
+            let highlightClass = '';
+            let medalIcon = '';
+            if (index === 0) {
+                highlightClass = 'first-place';
+                medalIcon = '<i class="fas fa-medal medal"></i>';
+            } else if (index === 1) {
+                highlightClass = 'second-place';
+                medalIcon = '<i class="fas fa-medal medal"></i>';
+            } else if (index === 2) {
+                highlightClass = 'third-place';
+                medalIcon = '<i class="fas fa-medal medal"></i>';
+            }
+    
+            return `<li class="${highlightClass}">${entry.name} : ${entry.moves} moves, ${entry.time} seconds ${medalIcon}</li>`;
+        }).join('');
+    
+        selectors.leaderboardSection.style.display = 'block'; // Show leaderboard section
+
+    };
+    const saveToLeaderboard = () => {
+        const playerName = selectors.input.value.trim();
+
+        if (playerName === '') {
+            alert('Please enter your name.');
+            return;
+        }
+
+        const leaderboard = JSON.parse(localStorage.getItem('mediumLeaderboard')) || [];
+        leaderboard.push({
+            name: playerName,
+            time: state.totalTime,
+            moves: state.totalFlips
+        });
+
+        localStorage.setItem('mediumLeaderboard', JSON.stringify(leaderboard));
+
+        selectors.input.value = '';
+        document.querySelector('.leaderboard-input').style.display = 'none';
+
+        alert('Your score has been saved to the leaderboard!');
+        displayLeaderboard(); // Display leaderboard after saving
+    };
+
+    const attachEventListeners = () => {
+        document.addEventListener('click', event => {
+            const eventTarget = event.target;
+            const eventParent = eventTarget.parentElement;
+
+            if (eventTarget.className.includes('card') && !eventParent.classList.contains('flipped')) {
+                flipCard(eventParent);
+            } else if (eventTarget.nodeName === 'BUTTON' && eventTarget.id === 'save-leaderboard') {
+                event.preventDefault(); // Prevent default action to stop any unintended behavior
+                saveToLeaderboard();
+            } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
+                startGame();
+            }
+        });
+    };
+
+    selectors.boardContainer.addEventListener('click', (e) => {
+        if (e.target.closest('.card')) {
+            flipCard(e.target.closest('.card'));
+        }
+    });
+
+    selectors.start.addEventListener('click', () => {
+        if (!state.gameStarted) {
+            startGame();
+        } else {
+            resetGame();
+        }
+    });
+
+    attachEventListeners();
+    generateGame();
+    displayLeaderboard();
+});
---
+++ xox.js
@@ -0,0 +1,206 @@
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('gameBoard');
+const statusElement = document.getElementById('status');
+const resultElement = document.getElementById('result');
+const restartButton = document.getElementById('restartButton');
+const winnerModal = document.getElementById('winnerModal');
+const winnerMessage = document.getElementById('winnerMessage');
+const newMatchButton = document.getElementById('newMatchButton');
+const closeModal = document.querySelector('.close');
+const startButton = document.getElementById('startButton');
+let oTurn, xTimer, oTimer, gameInterval;
+const turnTimeLimit = 20; // 20 seconds per player for the entire game
+const winThreshold = 3; // Number of wins required to win the match
+let back = document.getElementById('backBtn');
+
+startButton.addEventListener('click', startGame);
+restartButton.addEventListener('click', startGame);
+newMatchButton.addEventListener('click', () => {
+    winnerModal.style.display = 'none'; // Hide the modal
+    startGame(); // Start a new match
+});
+closeModal.addEventListener('click', () => {
+    winnerModal.style.display = 'none'; // Hide the modal
+});
+
+window.onclick = function(event) {
+    if (event.target === winnerModal) {
+        winnerModal.style.display = 'none'; // Hide the modal if the user clicks outside
+    }
+};
+
+function startGame() {
+    startButton.style.display = 'none'; // Hide the start button once the game starts
+    oTurn = false;
+    xTimer = turnTimeLimit;
+    oTimer = turnTimeLimit;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.classList.remove('winning-cell');
+        cell.textContent = ''; 
+        cell.classList.remove('cell-disabled'); 
+        cell.removeEventListener('click', handleClick); 
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    restartButton.style.display = 'inline-block';
+    clearInterval(gameInterval);
+    startGameTimer(); // Start the countdown timer for both players
+    displayWins();
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false, currentClass);
+        saveWinner(currentClass);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function endGame(draw, winnerClass = null) {
+    clearInterval(gameInterval);
+    if (draw) {
+        statusElement.textContent = "It's a Draw!";
+    } else {
+        statusElement.textContent = `${winnerClass.toUpperCase()} Wins!`;
+    }
+    disableCells();
+    restartButton.style.display = 'inline-block';
+}
+
+function disableCells() {
+    cellElements.forEach(cell => {
+        cell.classList.add('cell-disabled');
+    });
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+    cell.textContent = currentClass === X_CLASS ? 'X' : 'O';
+}
+
+function swapTurns() {
+    oTurn = !oTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
+    if (oTurn) {
+        board.classList.add(O_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
+}
+
+function checkWin(currentClass) {
+    let winningCombination = null;
+    WINNING_COMBINATIONS.some(combination => {
+        if (combination.every(index => cellElements[index].classList.contains(currentClass))) {
+            winningCombination = combination;
+            return true;
+        }
+        return false;
+    });
+
+    if (winningCombination) {
+        markWinningCombination(winningCombination, currentClass);
+        return true;
+    }
+
+    return false;
+}
+
+function markWinningCombination(combination, currentClass) {
+    combination.forEach(index => {
+        cellElements[index].classList.add('winning-cell');
+    });
+}
+
+function startGameTimer() {
+    const interval = 100; // Update every 100 milliseconds
+    let timeLeftX = turnTimeLimit;
+    let timeLeftO = turnTimeLimit;
+    
+    gameInterval = setInterval(() => {
+        if (oTurn) {
+            timeLeftO -= interval / 1000;
+        } else {
+            timeLeftX -= interval / 1000;
+        }
+
+        if (timeLeftX <= 0) {
+            clearInterval(gameInterval);
+            saveWinner(O_CLASS);  // O wins because X ran out of time
+            endGame(false, O_CLASS);  // End the game and announce O as the winner
+        } else if (timeLeftO <= 0) {
+            clearInterval(gameInterval);
+            saveWinner(X_CLASS);  // X wins because O ran out of time
+            endGame(false, X_CLASS);  // End the game and announce X as the winner
+        } else {
+            statusElement.innerHTML = `
+    <span class="turn-info">Current Turn: ${oTurn ? 'O' : 'X'}</span>
+    <div class="timer-info">
+        <span class="x-timer">X Time: ${timeLeftX.toFixed(1)}s</span> | 
+        <span class="o-timer">O Time: ${timeLeftO.toFixed(1)}s</span>
+    </div>
+`;
+        }
+    }, interval);
+}
+
+function saveWinner(winnerClass) {
+    const winner = winnerClass.toUpperCase();
+    let wins = JSON.parse(localStorage.getItem('ticTacToeWins')) || { X: 0, O: 0 };
+    wins[winner]++;
+    localStorage.setItem('ticTacToeWins', JSON.stringify(wins));
+
+    if (wins[winner] >= winThreshold) {
+        // Display modal with match result
+        winnerMessage.textContent = `${winner} wins the match!`;
+        winnerModal.style.display = 'block'; // Show the modal
+        localStorage.removeItem('ticTacToeWins');
+    } else {
+        displayWins();
+    }
+}
+
+function displayWins() {
+    const wins = JSON.parse(localStorage.getItem('ticTacToeWins')) || { X: 0, O: 0 };
+    resultElement.innerHTML = `
+    <span style="color: red;">X Wins: ${wins.X}</span> |
+    <span style="color: green;">O Wins: ${wins.O}</span>
+    `;
+}
+
+back.addEventListener('click', () => {
+    localStorage.removeItem('ticTacToeWins');
+    window.location.href = 'xoxhomepage.html';
+});
